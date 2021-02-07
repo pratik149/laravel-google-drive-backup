fed082e026d2398a91e776264c038cbfaa960dd4
Pratik Rane <pratikrane149@gmail.com>
2021-02-07 23:57:09 +0530

    add 'google' in the list of disks in backup.php config

--- config/backup.php
+++ config/backup.php
@@ -8,7 +8,7 @@ return [
          * The name of this application. You can use this name to monitor
          * the backups.
          */
-        'name' => env('APP_NAME', 'laravel-backup'),
+        'name' => env('APP_NAME', ''),
 
         'source' => [
 
@@ -116,6 +116,7 @@ return [
              * The disk names on which the backups will be stored.
              */
             'disks' => [
+				'google',
                 'local',
             ],
         ],
